9accae7c049a71f5acd0173b6f546a3322f27bc7
DESKTOP-BIHV00B\dengliang <anddl9395@163.com>
2019-01-29 13:52:22 +0800

    工具

---
+++ app/src/main/java/retrofitdemo/dengliang/com/retrofitdemo/MConfig.java
@@ -0,0 +1,12 @@
+package retrofitdemo.dengliang.com.retrofitdemo;
+
+/**
+ * Created by dengliang on 2019/1/28.
+ * 各种接口
+ */
+
+public class MConfig {
+    public static String BaseUrl = "https://lz.lcoce.com/customer/";//服务器基地址
+
+    public static String index = "index/index";//首页数据
+}
